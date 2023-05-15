734c0d532f13e71ea6d4df8b0fca9b5706875ccc
Maxwell Roberts <max031029@gmail.com>
2023-05-15 01:06:59 -0600

    landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,29 @@
 import "../styles/Landing.css"
+import search from "../images/search.png"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Landing () {
+    const [selected, setSelected] = useState(false)
+    const [query, setQuery] = useState("")
+
+    const nav = useNavigate()
+
     return (
         <div className="landing">
             <div className="title">
                 DogDogGo
             </div>
-            <div className="search_bar">
-                <input type="text" />
+            <div className={`search_bar ${selected ? "selected" : ""}`}>
+                <input value={query} onChange={(e) => {setQuery(e.target.value)}} type="text" placeholder="Search the web..." onFocus={() => {setSelected(true)}} onBlur={() => {setSelected(false)}}
+                onKeyDown={(e) => {
+                    if(e.key === "Enter") {
+                        if(query) {
+                            nav(`/search/${query}`)
+                        }
+                    }
+                }} />
+                <img src={search} alt="Search" />
             </div>
             <div className="misc">
                 The worlds first search engine for dogs.
