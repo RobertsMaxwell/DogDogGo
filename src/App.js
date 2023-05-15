ff908f2dd6e6115149071829050bf0e2cc280eab
Maxwell Roberts <max031029@gmail.com>
2023-05-15 01:07:56 -0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Landing from "./components/Landing"
+import Serp from "./components/Serp"
+import Footer from "./components/Footer"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <Landing />
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/search/:query" element={<Serp />} />
+      </Routes>
+      {/* <Footer /> */}
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import "../styles/Footer.css"
+
+export default function Footer () {
+    return (
+        <div className="footer">
+            <pre>Created by Maxwell Roberts - </pre><a href="https://github.com/robertsmaxwell" target="_blank">GitHub</a>
+        </div>
+    );
+}
