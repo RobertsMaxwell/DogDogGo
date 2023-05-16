1a6b39e539afb5cde418921fedc8cb6ecf4cd35e
Maxwell Roberts <max031029@gmail.com>
2023-05-15 20:25:14 -0600

    footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 export default function Footer () {
     return (
         <div className="footer">
-            <pre>Created by Maxwell Roberts - </pre><a href="https://github.com/robertsmaxwell" target="_blank">GitHub</a>
+            <pre>Created by </pre><a href="https://github.com/robertsmaxwell" target="_blank">Maxwell Roberts</a><pre> - </pre><a href="https://github.com/robertsmaxwell/DogDogGo" target="_blank">Repository</a>
         </div>
     );
 }
